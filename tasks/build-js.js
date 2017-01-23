1b8f0bffb08272f559159b28c15a3ff5106cd49a
Christian Schmelz <c.fritzkowski@gmail.com>
2017-01-23 15:14:27 +0100

    add uflify

--- tasks/build-js.js
+++ tasks/build-js.js
@@ -1,7 +1,10 @@
 const gulp        = require('gulp'),
     sourcemaps  = require('gulp-sourcemaps'),
     concat      = require('gulp-concat'),
+	uglify      = require("gulp-uglify"),
     notify      = require("gulp-notify");
+	copy 		= require("gulp-copy");
+	rename 		= require("gulp-rename");
 
 module.exports = function(gulp, config) {
     gulp.task('build-js', function () {
@@ -9,7 +12,10 @@ module.exports = function(gulp, config) {
             return gulp.src(config.jsSrcPath)
                 .pipe(sourcemaps.init())
                 .pipe(concat('bundle.js'))
-                .pipe(sourcemaps.write())
+				.pipe(gulp.dest(config.jsDistPath))
+				.pipe(rename("bundle.min.js"))
+				.pipe(uglify())
+                //.pipe(sourcemaps.write())
                 .pipe(gulp.dest(config.jsDistPath))
                 // You can comment this part out if you don't would like to have notifications on your desktop
                 .pipe(notify('JavaScript build and concatenated'));
